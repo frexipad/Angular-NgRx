b656de792eeec59260da3cd4a619e45e935a7c7c
frexipad <frexipad@gmail.com>
2021-02-18 12:15:03 +0100

    Add component pageNofound,his route, and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
